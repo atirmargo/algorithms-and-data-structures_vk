1d75af58accba4ec89c620d4bc6086ca6eaf3a1f
atirmargo <margaritalevcenko76@gmail.com>
2025-03-21 18:14:16 +0300

    update

--- reversePartOfTheArray.py
+++ reversePartOfTheArray.py
@@ -8,7 +8,7 @@ def reverseArray(nums, left, right):
     return nums
 def reversePartOfTheArray(k, nums):
     reverseArray(nums, 0, len(nums)-1)
-    reverseArray(nums, 0, k-1)
-    reverseArray(nums, k, len(nums)-1)
+    reverseArray(nums, 0, k % len(nums) -1)
+    reverseArray(nums, k % len(nums), len(nums)-1)
     return nums
 print(reversePartOfTheArray(k, nums))
