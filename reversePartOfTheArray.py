2bf120e4c1d5fee4ef430c4214aaf1912397c46f
atirmargo <margaritalevcenko76@gmail.com>
2025-03-21 12:14:20 +0300

    first

---
+++ reversePartOfTheArray.py
@@ -0,0 +1,14 @@
+nums = list(map(int, input().split()))
+k = int(input())
+def reverseArray(nums, left, right):
+    while left < right:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1
+    return nums
+def reversePartOfTheArray(k, nums):
+    reverseArray(nums, 0, len(nums)-1)
+    reverseArray(nums, 0, k-1)
+    reverseArray(nums, k, len(nums)-1)
+    return nums
+print(reversePartOfTheArray(k, nums))
