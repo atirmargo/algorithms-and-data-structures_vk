0413de22ba8b7e0d40e41e330c19a200b6bd502c
atirmargo <margaritalevcenko76@gmail.com>
2025-03-25 20:23:04 +0300

    com

--- reversePartOfTheArray.py
+++ reversePartOfTheArray.py
@@ -1,14 +1,16 @@
 nums = list(map(int, input().split()))
 k = int(input())
-def reverseArray(nums, left, right):
-    while left < right:
-        nums[left], nums[right] = nums[right], nums[left]
-        left += 1
-        right -= 1
-    return nums
-def reversePartOfTheArray(k, nums):
-    reverseArray(nums, 0, len(nums)-1)
-    reverseArray(nums, 0, k % len(nums) -1)
-    reverseArray(nums, k % len(nums), len(nums)-1)
-    return nums
-print(reversePartOfTheArray(k, nums))
+class Solution:
+    def reverseArray(self, nums, left, right):
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        return nums
+    def reversePartOfTheArray(self, k, nums):
+        self.reverseArray(nums, 0, len(nums)-1)
+        self.reverseArray(nums, 0, k % len(nums) -1)
+        self.reverseArray(nums, k % len(nums), len(nums)-1)
+        return nums
+solution = Solution()
+print(solution.reversePartOfTheArray(k, nums))
--- twoSum.py
+++ twoSum.py
@@ -3,16 +3,18 @@ ls = input().split()
 for i in ls:
     nums.append(int(i))
 target = int(input())
-def twoSum(nums, target):
-    left = 0
-    right = len(nums)-1
-    while left < right:
-        a = nums[left]+nums[right]
-        if a == target:
-            return [left, right]
-        elif a > target:
-            right -= 1
-        elif a < target:
-            left += 1
-    return[]
-print(twoSum(nums, target))
+class Solution:
+    def twoSum(self, nums, target):
+        left = 0
+        right = len(nums)-1
+        while left < right:
+            a = nums[left]+nums[right]
+            if a == target:
+                return [left, right]
+            elif a > target:
+                right -= 1
+            elif a < target:
+                left += 1
+        return[]
+solution = Solution()
+print(solution.twoSum(nums, target))
